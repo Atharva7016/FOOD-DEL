ac3a4051bb8bace6e6e0341bce3e87be9255097c
Atharva7016 <atharvapatil1107@gmail.com>
2025-10-04 00:51:29 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "https://food-del-gmd0.onrender.com";
+    const url = "https://food-del-backendd-sw5b.onrender.com";
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
